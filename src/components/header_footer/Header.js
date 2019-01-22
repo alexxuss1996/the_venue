1618f79782d167d2f94b4872c78ae9bea648481b
Olexiy Melnychuk <alekseymelnych132@gmail.com>
2019-01-22 16:12:07 +0200

    Add Drawer on  click

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -5,7 +5,19 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+import SideDrawer from './SideDrawer';
+
 export default class Header extends Component {
+  state = {
+    drawerOpen: false
+  };
+
+  toggleDrawer = value => {
+    this.setState({
+      drawerOpen: value
+    });
+  };
+
   render() {
     return (
       <AppBar
@@ -21,6 +33,17 @@ export default class Header extends Component {
             <div className='font_righteous header_logo_venue'>The Venue</div>
             <div className='header_logo_title'>Musical Events</div>
           </div>
+
+          <IconButton aria-label='Menu' color='inherit' onClick={value => this.toggleDrawer(value)}>
+            <MenuIcon />
+          </IconButton>
+
+          <SideDrawer
+            open={this.state.drawerOpen}
+            onClose={value => {
+              this.toggleDrawer(value);
+            }}
+          />
         </Toolbar>
       </AppBar>
     );
